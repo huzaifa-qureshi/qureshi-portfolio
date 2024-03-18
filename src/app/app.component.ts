4501276ef0c52302d376a55848259819acdf7b67
huzaifa-qureshi <huzaifa9819891@gmail.com>
2024-03-19 04:24:59 +0500

    mobile swiping functionality  test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -92,6 +92,13 @@ export class AppComponent implements AfterViewInit {
   }
   //#endregion
 
+  @HostListener('swipedown', ['$event'])
+  onSwipeDown(event: any) {
+    this.navservice.OnScroll(true).finally(() => {
+      this.isScrolling = false;
+    });
+  }
+
   toggleNav(event: boolean) {
     if (event == true) {
       this.collapseState = true;
